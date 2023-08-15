e70025c8d9c4f3740e2efca3e6c5348e493c0396
Sascha <Tohwa.gaming@gmail.com>
2023-08-15 19:36:13 +0200

    added grab and drop logic

--- 3D_Studyweek_0323/Assets/Scripts/Camera/TargetingCheck.cs
+++ 3D_Studyweek_0323/Assets/Scripts/Camera/TargetingCheck.cs
@@ -6,26 +6,52 @@ public class TargetingCheck : MonoBehaviour
     [SerializeField] private GameObject redObj;
     [SerializeField] private GameObject blueObj;
     [SerializeField] private GameObject yellowObj;
+    [SerializeField] private Transform _objectHolder;
+
+    Rigidbody grabbedRB;
+
+    private void Update()
+    {
+        if (grabbedRB)
+        {
+            grabbedRB.MovePosition(_objectHolder.transform.position);
+        }
+    }
 
     public void OnInteractHit(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
         {
-            RaycastHit hit;
-
-            Physics.Raycast(transform.position, transform.forward, out hit, 3);
-            if (hit.collider)
+            if(grabbedRB)
             {
-                if (hit.transform.tag == "inter" && GameManager.Instance.objectiveOne == hit.transform.name)
-                {
-                    hit.transform.gameObject.SetActive(false);
-                }
-                else if(hit.transform.tag == "NPC")
-                {
-                    ClearObjective();
-                }
-
+                grabbedRB.isKinematic = false;
+                grabbedRB = null;
             }
+            else
+            {
+                RaycastHit hit;
+
+                Physics.Raycast(transform.position, transform.forward, out hit, 3);
+                if (hit.collider)
+                {
+                    if (hit.transform.tag == "inter" && GameManager.Instance.objectiveOne == hit.transform.name)
+                    {
+                        //hit.transform.gameObject.SetActive(false);
+                        grabbedRB = hit.collider.gameObject.GetComponent<Rigidbody>();
+
+                        if (grabbedRB)
+                        {
+                            grabbedRB.isKinematic = true;
+                        }
+                    }
+                    else if (hit.transform.tag == "NPC")
+                    {
+                        ClearObjective();
+                    }
+
+                }
+            }
+            
         }
     }
 
